fc82a296fc2b0d53034e878799ee07036e858170
Karthikadevi M <karthikadevi056@gmail.com>
2025-04-12 00:19:22 +0530

    fix for npm build failure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Weather from './Components/Weather'
+import Weather from './Components/Weather';
 
 function App() {
   return (
     <div className='app'>
       <Weather/>
-   
     </div>
   )
 }
